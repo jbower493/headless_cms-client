17a80a2d3b1e5306580c28f1b7677efb411623c5
Jamie Bower <j_bower@hotmail.co.uk>
2021-04-12 22:09:32 +1000

    continued working on the content modal

--- src/Components/Modal/ContentModal/ContentModal.js
+++ src/Components/Modal/ContentModal/ContentModal.js
@@ -1,13 +1,57 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 
+const fields = [
+  {
+    name: 'title',
+    type: 'text',
+    value: 'this is the title'
+  },
+  {
+    name: 'body text',
+    type: 'text',
+    value: 'this is the body text'
+  },
+  {
+    name: 'image_ref',
+    type: 'number',
+    value: '34'
+  }
+];
+
 const ContentModal = (props) => {
   // useEffect(() => {
 
   // }, []);
 
   return (
-    <div className="modal"></div>
+    <div className="modal">
+      <div className="modal__header">
+        <h2 className="modal__title">New Article</h2>
+      </div>
+
+      <div className="modal__body">
+        <h3 className="content-modal__title">Fields</h3>
+        <div className="content-modal__headings">
+          <h4 className="content-modal__heading">Field</h4>
+          <h4 className="content-modal__heading">Type</h4>
+          <h4 className="content-modal__heading">Value</h4>
+        </div>
+
+        <ul className="content-modal__list">
+          {
+            fields.map(field => (
+              <li className="content-modal__list-item">A field</li>
+            ))
+          }
+        </ul>
+      </div>
+
+      <div className="modal__footer">
+        <button className="btn btn--primary">Publish</button>
+        <button className="btn btn--outlined">Cancel</button>
+      </div>
+    </div>
   );
 };
 
