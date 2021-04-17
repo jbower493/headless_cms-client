d51e8a0a5125c31fe186ef5067f68361ff9fa03b
Jamie Bower <j_bower@hotmail.co.uk>
2021-04-17 12:36:41 +1000

    finished new content modal now, except wen new content is created it doesn't re-render the content list component, need to sort this out next

--- src/redux/actions/modalActions.js
+++ src/redux/actions/modalActions.js
@@ -1,4 +1,4 @@
-import { SET_MODAL, REQUESTING_MODAL_DATA, RECEIVED_MODAL_DATA } from './types';
+import { SET_MODAL, REQUESTING_MODAL_DATA, RECEIVED_MODAL_DATA, SET_SENDING_TRUE, SET_SENDING_FALSE } from './types';
 
 export const setModalAC = modal => {
   return {
@@ -16,5 +16,17 @@ export const requestingModalDataAC = () => {
 export const receivedModalDataAC = () => {
   return {
     type: RECEIVED_MODAL_DATA
+  };
+};
+
+export const setSendingTrueAC = () => {
+  return {
+    type: SET_SENDING_TRUE
+  };
+};
+
+export const setSendingFalseAC = () => {
+  return {
+    type: SET_SENDING_FALSE
   };
 };
