8b5b25b8a51ffe3281eddd9af316e47f34edb7b5
Jamie Bower <j_bower@hotmail.co.uk>
2021-09-28 22:27:18 +1000

    feat: Started content types table

--- src/containers/ContentTypes/index.js
+++ src/containers/ContentTypes/index.js
@@ -6,11 +6,36 @@ import { withRouter } from 'react-router-dom';
 /*----------Components, sections, modules----------*/
 
 /*----------Shared components----------*/
+import Table from 'components/Table';
 
 /*----------Actions----------*/
 
 /*----------Component start----------*/
 class ContentTypes extends Component {
+  constructor(props) {
+    super(props);
+
+    this.getContentTypesMatrix = this.getContentTypesMatrix.bind(this);
+  }
+
+  getContentTypesMatrix(data) {
+    if (data?.length <= 0) return [];
+
+    return data.map(item => {
+
+
+      return {
+        hello: 'hello',
+        actions: [
+          {
+            style: 'outline button',
+            onClick: () => alert('Hey'),
+            text: 'Say Hey'
+          }
+        ]
+      }
+    })
+  }
 
   /*----------Lifecycle methods----------*/
   componentDidMount() {
@@ -22,11 +47,22 @@ class ContentTypes extends Component {
   }
 
   render() {
+    const { getContentTypesMatrix } = this;
 
     /*----------Render component----------*/
     return (
       <div className={`contentTypes`}>
-        Content Tyes
+        <h3 className={`contentTypes__heading`}>Content Types</h3>
+        <section className={`contentTypes__container`}>
+          <Table
+            status={'success'}
+            head={[
+              { heading: 'Content Type Name' },
+              { heading: '', type: 'actions' }
+            ]}
+            body={getContentTypesMatrix([1,2,3,4,5])}
+           />
+        </section>
       </div>
     );
   }
